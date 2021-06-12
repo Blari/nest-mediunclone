0a5fb82e3f02bb107b6c281b27b9ee231b25648d
blarior@gmail.com <4fm4KsXyByNg>
2021-06-13 02:01:09 +0300

    Add tags module/controller

---
+++ src/tag/tag.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from "@nestjs/common";
+
+@Controller('tags')
+export class TagController {
+
+    @Get()
+    findAll() {
+        return ['dragons', 'aliens']
+    }
+
+}
