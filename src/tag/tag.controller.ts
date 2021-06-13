8d92ae812fbcce1ecbdf1f274f0cb014bddade48
blarior@gmail.com <4fm4KsXyByNg>
2021-06-13 03:18:06 +0300

    Add postgres db

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get } from '@nestjs/common';
+import { TagEntity } from './tag.entity';
+import { TagService } from './tag.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
 
-    @Get()
-    findAll() {
-        return ['dragons', 'aliens']
-    }
-
+  @Get()
+  async findAll(): Promise<{ tags: string[] }> {
+    const tags = await this.tagService.findAll();
+    return { tags: tags.map((tag) => tag.name) };
+  }
 }
