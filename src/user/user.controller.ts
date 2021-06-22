f563019103b0aef2b80b3c517a128b1229b4d592
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2021-06-22 14:17:33 +0300

    Added user dto

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/createUser.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post('users')
-  async createUser(@Body('user') createUserDto: any): Promise<any> {
-    return this.userService.createUser();
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<any> {
+    return this.userService.createUser(createUserDto);
   }
 }
