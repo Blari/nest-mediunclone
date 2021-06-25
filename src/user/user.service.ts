0a952fbe322d8ab424d50ffcb060e53cd3777e53
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2021-06-25 11:23:01 +0300

    Added user validation

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { UserEntity } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -15,6 +15,20 @@ export class UserService {
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      email: createUserDto.email,
+    });
+    const userByUserName = await this.userRepository.findOne({
+      username: createUserDto.username,
+    });
+
+    if (userByEmail || userByUserName) {
+      throw new HttpException(
+        'Email or Username are taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     return await this.userRepository.save(newUser);
