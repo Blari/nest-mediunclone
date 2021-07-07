bb8d0a69216fb939ebf291899da311c22f96dfde
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2021-07-07 14:40:33 +0300

    Added user auth middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import ormconfig from './ormconfig';
 import { TagModule } from './tag/tag.module';
 import { UserModule } from './user/user.module';
+import { AuthMiddleware } from './user/middlewares/auth.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
