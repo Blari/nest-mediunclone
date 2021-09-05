eeacd66fa4cc916de5e5c745eb4fa779a582441e
blarior@gmail.com <4fm4KsXyByNg>
2021-09-05 23:48:53 +0300

    Update dependencies

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from 'typeorm';
 const config: ConnectionOptions = {
   type: 'postgres',
   host: 'localhost',
-  port: 5433,
+  port: 5432,
   username: 'mediumclone',
   password: '123',
   database: 'mediumclone',
